399142991e875287e408ddfe8f58b2a42df81e75
caotrinhthinh <caotrinhthinh@gmail.com>
2025-08-16 14:52:39 +0700

    first commit

---
+++ src/main/java/com/example/CTT_shops/CttShopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.CTT_shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CttShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CttShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CTT_shops/CttShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CTT_shops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CttShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
